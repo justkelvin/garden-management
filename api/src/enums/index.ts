35659d74f70453815d9501b3e28a3a3eb5ff33b6
justkelvin <muia.muinde@s.karu.ac.ke>
2024-05-27 13:40:31 +0300

    add enum values

---
+++ api/src/enums/index.ts
@@ -0,0 +1,15 @@
+export enum BloodGroup {
+    'O+', 'O-', 'B+', 'B-', 'AB+', 'AB-', 'A+', 'A-'
+}
+
+enum USER_RULES {
+    "ADMIN",
+    "CUSTOMER",
+    "GARDENER"
+}
+export enum AuthUser {
+    ADMIN = 'admin',
+    GARDENER = 'gardener',
+    CUSTOMER = 'customer',
+    SUPER_ADMIN = 'super_admin'
+}
